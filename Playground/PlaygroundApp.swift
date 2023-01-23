2e40fb5b603e668901edd94ff67742ee13a6f4b6
Mikey Ward <mw@mikey.io>
2023-01-23 09:35:17 -0500

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Mikey Ward on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
